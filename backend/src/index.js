3e2e65b1a1d195daa9ee9be2310f2926708deee4
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-06 19:37:48 +0530

    create message model, controller, router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 // const express = require("express");
 import express from "express";
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
@@ -12,7 +13,8 @@ const PORT=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth',authRoutes)
+app.use('/api/auth',authRoutes);
+app.use('/api/message',messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port PORT:"+PORT);
