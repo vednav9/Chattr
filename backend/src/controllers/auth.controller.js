d99bbf1aa1c0cf89f2121e083565689495f8bafc
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-06 19:02:36 +0530

    Implement authentication features: add profile update and checkAuth routes, integrate Cloudinary

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,8 +5,8 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    if (!fullName||!email||!password) {
-        return res.status(400).json({message:"All fields are required"});
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
     if (password.length < 6) {
       return res
@@ -28,53 +28,80 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       // Generate JWT token
-      generateToken(newUser._id, res)
+      generateToken(newUser._id, res);
       await newUser.save();
       res.status(201).json({
-        _id:newUser._id,
-        fullName:newUser.fullName,
-        email:newUser.email,
-        profilePic:newUser.profilePic,
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
       });
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
   } catch (error) {
     console.log("Error is signup controller", error.message);
-    res.status(500).json({message:"Invalid Server Error"});    
+    res.status(500).json({ message: "Invalid Server Error" });
   }
 };
 
 export const login = async (req, res) => {
-  const {email, password}=req.body
-    try {
-    const user=await User.findOne({email})
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
     if (!user) {
-        return res.status(400).json({message:"Invalid credentials"})
+      return res.status(400).json({ message: "Invalid credentials" });
     }
-    const isPasswordCorrect =await bcrypt.compare(password,user.password)
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
-        return res.status(400).json({message:"Invalid credentials"})
+      return res.status(400).json({ message: "Invalid credentials" });
     }
-    generateToken(user._id, res)
+    generateToken(user._id, res);
     res.status(200).json({
-        _id:user._id,
-        fullName:user.fullName,
-        email:user.email,
-        profilePic:user.profilePic,
-    })
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
   } catch (error) {
     console.log("Error in login controller", error.message);
-    res.status(500).json({message:"Invalid Server Error"});
+    res.status(500).json({ message: "Invalid Server Error" });
   }
 };
 
 export const logout = (req, res) => {
   try {
-    res.cookie('jwt','',{maxAge:0})
-    res.status(200).json({message:"Logged out successfully"})
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in login controller", error.message);
-    res.status(500).json({message:"Invalid Server Error"});
+    res.status(500).json({ message: "Invalid Server Error" });
   }
 };
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic}=req.body;
+        const userId=req.user._id;
+        if (!profilePic) {
+            return res.status(400).json({message: "Profile pic is required"});
+        }
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log('error in updated profile:',error);
+        res.status(500).json({message:"Internal server error"});        
+    }
+};
+
+
+export const checkAuth=(req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message:"Internal Server Error"});        
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,17 @@ import express from "express";
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 const app = express();
 const PORT=process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/auth',authRoutes)
+
 app.listen(PORT, () => {
   console.log("Server is running on port PORT:"+PORT);
   connectDB();
