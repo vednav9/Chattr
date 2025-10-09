3ff7ecf7fd24f966322b8e6f677551d6d4107e0f
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-09 14:44:05 +0530

    complete frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,51 @@
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import Profile from "./pages/Profile";
-import { axiosInstance } from "./lib/axios";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const { authUser, checkAuth, isCheckedAuth } = useAuthStore();
-  useEffect(
-    () => {
-      checkAuth();
-    },
-    { checkAuth }
-  );
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log({ onlineUsers });
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   console.log({ authUser });
 
-  if (!isCheckedAuth && !authUser) {
-    <div className="flex items-center justify-center h-screen">
-      <Loader className="size-10 animate-spin" />
-    </div>;
-  }
-  const navigate = useNavigate();
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={authUser ? <Home /> : navigate("/login")} />
-        <Route path="/signup" element={!authUser ? <SignUp /> : navigate("/")} />
-        <Route path="/login" element={!authUser ? <Login /> : navigate("/")} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/profile" element={authUser ? <Profile /> : navigate("/login")} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
-}
-
+};
 export default App;
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5001/api",
-    withCredentials:true,
-})
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  withCredentials: true,
+});
